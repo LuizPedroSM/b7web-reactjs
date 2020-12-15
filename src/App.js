5c70207132f45a91fc68e272c0b314a5694231f6
Luiz Pedro <luizpsmoreira@gmail.com>
2020-12-15 19:26:19 -0300

    Ciclo de Vida(useEffect)

--- src/App.js
+++ src/App.js
@@ -1,71 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
-const Input = styled.input`
-  width: 400px;
-  height: 30px;
-  font-size: 16px;
-  padding: 10px;
-  border: 1px solid #000;
-  margin: 10px 0;
-`;
-
 function App() {
-  const [conta, setConta] = useState("");
-  const [perGorjeta, setPerGorjeta] = useState("");
+  const [contagem, setContagem] = useState(0);
+
+  useEffect(() => {
+    if (contagem === 0) {
+      document.title = "Começou a brincadeira";
+    } else {
+      document.title = "Contagem: " + contagem;
+    }
+
+    // Quando o componente é destruído a func de retorno é executada
+    // return ()=>{
+
+    // }
+  }, [contagem]);
+
+  function aumentarAction() {
+    setContagem(contagem + 1);
+  }
+
   return (
     <>
-      <h2>Calculadora de Gorjeta</h2>
-      <label htmlFor="conta">
-        Quanto deu a conta? <br />
-        <Input
-          id="conta"
-          type="number"
-          value={conta}
-          onChange={e => setConta(Number(e.target.value))}
-        />
-      </label>
-      <br />
-      <label htmlFor="perGorjeta">
-        Qual a porcentagem de gorjeta? <br />
-        <Input
-          id="perGorjeta"
-          type="number"
-          value={perGorjeta}
-          onChange={e => setPerGorjeta(Number(e.target.value))}
-        />
-      </label>
-      <hr />
-      {conta > 0 && perGorjeta > 0 && (
-        <>
-          <p>
-            Sub-total:
-            {conta.toLocaleString("pt-Br", {
-              style: "currency",
-              currency: "BRL"
-            })}
-          </p>
-          <p>
-            Gorjeta ({perGorjeta}%):
-            {((conta * perGorjeta) / 100).toLocaleString("pt-Br", {
-              style: "currency",
-              currency: "BRL"
-            })}
-          </p>
-          <p>
-            <b>
-              Total:
-              {Number((conta * perGorjeta) / 100 + conta).toLocaleString(
-                "pt-Br",
-                {
-                  style: "currency",
-                  currency: "BRL"
-                }
-              )}
-            </b>
-          </p>
-        </>
-      )}
+      <h1>Contagem: {contagem}</h1>
+      <button onClick={aumentarAction}>Aumentar Número</button>
     </>
   );
 }
