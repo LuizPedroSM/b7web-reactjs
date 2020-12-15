81934ad66d4acc2499aa54fa55b08bae91553c74
Luiz Pedro <luizpsmoreira@gmail.com>
2020-12-15 18:35:04 -0300

    Campo de Input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-function App() {
-  const [contagem, setContagem] = useState(0);
+const Input = styled.input`
+  width: 400px;
+  height: 30px;
+  font-size: 16px;
+  padding: 10px;
+  border: 1px solid #000;
+`;
 
-  const botaoAction = () => {
-    setContagem(contagem + 1);
+function App() {
+  const [texto, setTexto] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleInput = e => {
+    setTexto(e.target.value);
+  };
+  const handleEmailInput = e => {
+    setEmail(e.target.value);
+  };
+
+  const handleButton = e => {
+    alert(email + " - " + password);
   };
 
   return (
     <>
-      <div>{contagem} vezes</div>
-      <button onClick={botaoAction}>Clique para aumentar</button>
+      <Input type="text" value={texto} onChange={handleInput} />
+      <p>{texto}</p>
+      <p>{texto.length} caracteres</p>
+
+      <Input
+        placeholder="Digite um e-mail"
+        type="email"
+        value={email}
+        onChange={handleEmailInput}
+      />
+
+      <Input
+        placeholder="Digite uma senha"
+        type="password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleButton}>Dizer</button>
     </>
   );
 }
