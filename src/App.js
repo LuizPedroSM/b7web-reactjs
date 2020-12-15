f34417e5dd82b8b731b29936af59bc4308af65f1
Luiz Pedro <luizpsmoreira@gmail.com>
2020-12-15 00:39:51 -0300

    Estilizacao com StyledComponents(3/3)

--- src/App.js
+++ src/App.js
@@ -7,29 +7,27 @@ const Site = styled.div`
   background-color: #00ff00;
 `;
 
-const Title = styled.h1`
-  color: #ff0000;
-  font-size: 18px;
-`;
-
 const Botao = styled.button`
   font-size: 19px;
   padding: 10px 15px;
-  background-color: ${props => (props.ativo == true ? "#fff" : "#ccc")};
-  color: ${props => props.color || "#0f0"};
+  border: 3px solid #f00;
+  color: #f00;
+  background-color: #fff;
+  margin: 5px;
+  border-radius: 5px;
+`;
+
+const BotaoPequeno = styled(Botao)`
+  padding: 5px 10px;
+  font-size: 16px;
+  border: 2px dashed #f00;
 `;
 
 function App() {
   return (
     <Site>
-      <Title>Titulo</Title>
-      <Botao ativo={true} color="#f00">
-        Clique aqui
-      </Botao>
-      <Botao ativo={false} color="#00f">
-        Clique aqui
-      </Botao>
       <Botao>Clique aqui</Botao>
+      <BotaoPequeno>Clique aqui</BotaoPequeno>
     </Site>
   );
 }
