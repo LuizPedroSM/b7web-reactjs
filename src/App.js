bb687f81f34a15d2dbee8630ea3eb7e6adefa8e4
Luiz Pedro <luizpsmoreira@gmail.com>
2020-12-14 23:44:28 -0300

    Entendendo o JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from "react";
 
-// React como class
-// class App extends React.Component {
-//   render() {
-//     return <h1>Testando 1, 2, 3...</h1>;
-//   }
-// }
+function formatarNome(usuario) {
+  return usuario.nome + " " + usuario.sobreNome;
+}
+function App() {
+  let usuario = {
+    nome: "Bonieky",
+    sobreNome: "Lacerda"
+  };
 
-// React como func
-// function App() {
-//   return <h1>Testando 4, 5, 6...</h1>;
-// }
+  let imagem = "https://www.google.com/logos/google.jpg";
 
-// React em arrow func
-// let App = () => {
-//   return <h1>Testando 7, 8, 9</h1>;
-// };
-
-// React em arrow func resumido
-let App = () => <h1>Testando 10, 11, 12</h1>;
+  return (
+    <>
+      <div>Meu nome é {formatarNome(usuario)}</div>
+      <img src={imagem} alt="Logo do Google" style={{ width: 200 }} />
+    </>
+  );
+}
 
 export default App;
