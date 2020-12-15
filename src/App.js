4b2105bebed0f3e808d7f698a76cd903970a068f
Luiz Pedro <luizpsmoreira@gmail.com>
2020-12-14 23:34:16 -0300

    Classes e funcoes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      Bem vindo
-    </div>
-  );
-}
+// React como class
+// class App extends React.Component {
+//   render() {
+//     return <h1>Testando 1, 2, 3...</h1>;
+//   }
+// }
+
+// React como func
+// function App() {
+//   return <h1>Testando 4, 5, 6...</h1>;
+// }
+
+// React em arrow func
+// let App = () => {
+//   return <h1>Testando 7, 8, 9</h1>;
+// };
+
+// React em arrow func resumido
+let App = () => <h1>Testando 10, 11, 12</h1>;
 
 export default App;
