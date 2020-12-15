acfa5c4cb33cb3f4d6446f3fa22c203ed404c81f
Luiz Pedro <luizpsmoreira@gmail.com>
2020-12-15 19:15:32 -0300

    Ex: Calculadora de Gorjeta

--- src/App.js
+++ src/App.js
@@ -7,30 +7,65 @@ const Input = styled.input`
   font-size: 16px;
   padding: 10px;
   border: 1px solid #000;
+  margin: 10px 0;
 `;
 
 function App() {
-  const [email, setEmail] = useState("");
-  const [isLogged, setIsLogged] = useState(false);
+  const [conta, setConta] = useState("");
+  const [perGorjeta, setPerGorjeta] = useState("");
   return (
     <>
-      <Input
-        placeholder="Digite um e-mail"
-        type="email"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-
-      {email.length > 0 && (
+      <h2>Calculadora de Gorjeta</h2>
+      <label htmlFor="conta">
+        Quanto deu a conta? <br />
+        <Input
+          id="conta"
+          type="number"
+          value={conta}
+          onChange={e => setConta(Number(e.target.value))}
+        />
+      </label>
+      <br />
+      <label htmlFor="perGorjeta">
+        Qual a porcentagem de gorjeta? <br />
+        <Input
+          id="perGorjeta"
+          type="number"
+          value={perGorjeta}
+          onChange={e => setPerGorjeta(Number(e.target.value))}
+        />
+      </label>
+      <hr />
+      {conta > 0 && perGorjeta > 0 && (
         <>
           <p>
-            {email.length} caractere{email.length !== 1 ? "s" : ""}
+            Sub-total:
+            {conta.toLocaleString("pt-Br", {
+              style: "currency",
+              currency: "BRL"
+            })}
           </p>
-          <p>Algo a mais</p>
+          <p>
+            Gorjeta ({perGorjeta}%):
+            {((conta * perGorjeta) / 100).toLocaleString("pt-Br", {
+              style: "currency",
+              currency: "BRL"
+            })}
+          </p>
+          <p>
+            <b>
+              Total:
+              {Number((conta * perGorjeta) / 100 + conta).toLocaleString(
+                "pt-Br",
+                {
+                  style: "currency",
+                  currency: "BRL"
+                }
+              )}
+            </b>
+          </p>
         </>
       )}
-
-      {isLogged ? <button>Sair</button> : <button>Fazer Login</button>}
     </>
   );
 }
